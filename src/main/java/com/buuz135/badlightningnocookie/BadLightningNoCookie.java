a58a99dd5e6e76f2b46c0d742d9aa077653e0f33
Buuz135 <buuz135@hotmail.com>
2021-11-29 18:32:07 +0100

    Initial Commit

---
+++ src/main/java/com/buuz135/badlightningnocookie/BadLightningNoCookie.java
@@ -0,0 +1,51 @@
+package com.buuz135.badlightningnocookie;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.SimpleSound;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("badlightningnocookie")
+public class BadLightningNoCookie {
+
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public BadLightningNoCookie() {
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> MinecraftForge.EVENT_BUS.addListener(this::playSound));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void playSound(PlaySoundEvent event){
+        if (SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER.getRegistryName().equals(event.getSound().getSoundLocation()) && event.getSound() instanceof SimpleSound){
+            SimpleSound simpleSound = (SimpleSound) event.getSound();
+            if (Minecraft.getInstance().player != null && !Minecraft.getInstance().player.getPosition().withinDistance(new BlockPos(simpleSound.getX(), simpleSound.getY(), simpleSound.getZ()), 128)){
+                event.setResultSound(null);
+            }
+        }
+
+    }
+
+}
